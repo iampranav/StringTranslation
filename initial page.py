e8b77db5da35a638a9fdb7cce6dc28b1a1673156
iampranav <vanpr7@gmail.com>
2016-01-14 12:10:55 +0530

    Replace the translated lines in original xml

--- initial page.py
+++ initial page.py
@@ -11,11 +11,14 @@ for i in range(0, size-1):
         if '\">' in item:
             var = item[item.find("\">") + len("\">"):]
             infile.append(var)
+fl.close()
 
 baseUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate"
 accessKey = "trnsl.1.1.20160112T160751Z.931856e92a58d2f0.710111159f3a1b35285d8529a9beb1be4b069160"
 lang = ["de", "ru", "es"]
 
+transline = []
+m = 0
 
 for k in range(0, len(infile)-1):
     for l in range(0, len(lang)-1):
@@ -24,4 +27,13 @@ for k in range(0, len(infile)-1):
 
         for key, value in data.items():
             if key == 'text':
-                print value[0]
+                transline[m] = value
+                m = m+1
+        
+fl2 = open("/home/pranav/Downloads/strings.xml", "r")
+unedlines = fl2.readlines()
+sz2 = len(unlines)
+for n in range (0,sz2-1):
+    finalout[n] = unedlines[n].replace(infile[n],transline[n])
+    
+print finalout
